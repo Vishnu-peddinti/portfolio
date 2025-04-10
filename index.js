d5deea594a9e61c0de28522017f4185234f38dd0
vishnu-peddinti <vishnupeddinti53@gmail.com>
2025-04-10 14:36:55 +0530

    added styles, script files

---
+++ index.js
@@ -0,0 +1,24 @@
+function toggleTheme() {
+    const body = document.body;
+    const themeIcon = document.getElementById('theme-icon');
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        themeIcon.style.backgroundImage = 'url("./icons/sun.png")';
+    } else {
+        themeIcon.style.backgroundImage = 'url("./icons/moon.png")';
+    }
+}
+window.addEventListener("DOMContentLoaded", () => {
+const movableDiv = document.querySelector('.moon');
+const follower = document.querySelector('.follow');
+document.addEventListener('mousemove', (event) => {
+    const mouseX = event.pageX;
+    const mouseY = event.pageY;
+
+    const offsetX = (mouseX / window.innerWidth - 0.5) * 20;
+    const offsetY = (mouseY / window.innerHeight - 0.5) * 20;
+
+    movableDiv.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+    follower.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
+    follower.style.opacity = 1;
+})});
